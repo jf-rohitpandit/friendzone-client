8f55b7e9f06d679a084507197ca760d46f640e57
Rohit Pandit <rohit0311@outlook.com>
2021-04-16 15:03:34 +0530

    adds reducers/index.js and reducers/userReducer file

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,14 @@
+import {} from '../constants/userConstants';
+
+const initialState = {
+	loading: false,
+	userInfo: null,
+	error: null,
+};
+
+export const userRegisterReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
