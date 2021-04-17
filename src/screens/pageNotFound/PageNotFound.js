0294e77de1191fafde4b1f3f1187bda0e8606346
Rohit Pandit <rohit0311@outlook.com>
2021-04-17 17:24:36 +0530

    adds pageNotFound.js

--- src/screens/pageNotFound/PageNotFound.js
+++ src/screens/pageNotFound/PageNotFound.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import classes from './PageNotFound.module.css';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
-	return <div>404! Page not found</div>;
+	return (
+		<div className={`container d-flex flex-column  ${classes.looks} `}>
+			<h1>404!</h1>
+			<h4>Page not found</h4>
+			<p>
+				return back to <Link to='/'>Home</Link>
+			</p>
+		</div>
+	);
 };
 
 export default PageNotFound;
