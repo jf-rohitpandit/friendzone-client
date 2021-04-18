4b64d1e9b552f29d11bab3d34c98fc2c6d56d114
Rohit Pandit <rohit0311@outlook.com>
2021-04-18 09:59:34 +0530

    adds dummy chat page

--- src/screens/chat/Chat.js
+++ src/screens/chat/Chat.js
@@ -1,8 +1,76 @@
 import React from 'react';
 import classes from './Chat.module.css';
+import girl from '../home/girl1.jpg';
 
 const Chat = () => {
-	return <div>Chat page</div>;
+	return (
+		<div className='container'>
+			<h2>Chat</h2>
+			<hr />
+			<div className=' bg-primary p-2'>
+				<div className='d-flex align-items-center m-2'>
+					<img src={girl} alt='' className={classes.avtar} />
+					<h6 className='pl-2 text-white'>Full Name</h6>
+				</div>
+				<div className={classes.chat}>
+					<div className=''>
+						<div className={classes.message}>
+							This is some message from the girl one so thit is some message
+							from the other user This is some extra message content from the
+							other user and
+						</div>
+					</div>
+					<div className=''>
+						<div className={`${classes.me} ${classes.message}`}>
+							This is some message from the girl one so
+						</div>
+					</div>
+					<div className=''>
+						<div className={classes.message}>
+							This is some message from the girl one so thit is some message
+							from the other user This is some extra message content from the
+							other user and
+						</div>
+					</div>
+					<div className=''>
+						<div className={`${classes.me} ${classes.message}`}>
+							This is some message from the girl one so
+						</div>
+					</div>
+					<div className=''>
+						<div className={classes.message}>
+							This is some message from the girl one so thit is some message
+							from the other user This is some extra message content from the
+							other user and
+						</div>
+					</div>
+					<div className=''>
+						<div className={`${classes.me} ${classes.message}`}>
+							This is some message from the girl one so
+						</div>
+					</div>
+					<div className=''>
+						<div className={classes.message}>
+							This is some message from the girl one so thit is some message
+							from the other user This is some extra message content from the
+							other user and
+						</div>
+					</div>
+					<div className=''>
+						<div className={`${classes.me} ${classes.message}`}>
+							This is some message from the girl one so
+						</div>
+					</div>
+				</div>
+			</div>
+			<form className=' form-group  mt-2 mt-b2 '>
+				<div className='d-flex'>
+					<input type='text' className='form-control' />
+					<button className='btn btn-info'>Send</button>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default Chat;
