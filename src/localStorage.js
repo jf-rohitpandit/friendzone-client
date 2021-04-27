f59ab26103fe8e42cde6edac0987a5aabf19e9cc
Rohit Pandit <rohit0311@outlook.com>
2021-04-27 12:07:17 +0530

    adds deleteToken in localStorage.js

--- src/localStorage.js
+++ src/localStorage.js
@@ -9,8 +9,17 @@ export const loadToken = () => {
 
 export const saveToken = (token) => {
 	try {
+		console.log('localStorage.js ', token);
 		const tokenString = JSON.stringify(token);
 		localStorage.setItem('token', tokenString);
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+export const deleteToken = () => {
+	try {
+		localStorage.setItem('token', null);
 	} catch (error) {
 		console.log(error);
 	}
