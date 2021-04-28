ba491aeee0bbcc8d90698e909add3b19aea97826
Rohit Pandit <rohit0311@outlook.com>
2021-04-28 16:30:44 +0530

    removes faker and adds loads the userInfo from the server

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -1,31 +1,26 @@
+import axios from 'axios';
 import {
 	LOAD_USER_FAILURE,
 	LOAD_USER_REQUEST,
 	LOAD_USER_SUCCESS,
 } from '../constants/userConstants';
-import faker from 'faker';
 
 export const loadUser = () => async (dispatch) => {
 	try {
 		dispatch({ type: LOAD_USER_REQUEST });
 
-		const image = faker.image.avatar();
-		const name = faker.name.firstName() + faker.name.lastName();
-		const country = faker.address.country();
-		const age = Math.floor(Math.random() * 20 + 18);
-		const aboutMe = faker.lorem.lines();
-		const gender = faker.name.gender();
+		const result = await axios.get('http://localhost:5000/home');
 
-		const userInfo = {
-			image,
-			name,
-			country,
-			age,
-			aboutMe,
-			gender,
-		};
+		// const userInfo = {
+		// 	image,
+		// 	name,
+		// 	country,
+		// 	age,
+		// 	aboutMe,
+		// 	gender,
+		// };
 
-		dispatch({ type: LOAD_USER_SUCCESS, payload: userInfo });
+		dispatch({ type: LOAD_USER_SUCCESS, payload: result.data.userInfo });
 	} catch (error) {
 		dispatch({ type: LOAD_USER_FAILURE, error: error });
 	}
